42b5f8e00685fcf97514a3e1be3e9faaa66251a9
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-10-18 15:46:03 +0200

    fixing cors problems

--- app.js
+++ app.js
@@ -3,33 +3,25 @@ require('./db');
 require('./config')(app);
 const express = require('express');
 const app = express();
-var cors = require('cors'); //import cors module
 
 // 👇 MIDDLEWARE MISSING
-var whitelist = [ 'http://localhost:8000', 'https://reto-web-phones-cli.herokuapp.com/' ]; //white list consumers
-var corsOptions = {
-	origin: function(origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else {
-			callback(null, false);
-		}
-	},
-	methods: [ 'GET', 'PUT', 'POST', 'DELETE', 'OPTIONS' ],
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-	credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-	allowedHeaders: [
-		'Content-Type',
-		'Authorization',
-		'X-Requested-With',
-		'device-remember-token',
-		'Access-Control-Allow-Origin',
-		'Origin',
-		'Accept'
-	]
-};
+app.use(function(req, res, next) {
+	// Website you wish to allow to connect
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-app.use(cors(corsOptions)); //adding cors middleware to the express with above configurations
+	// Request methods you wish to allow
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+	// Request headers you wish to allow
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	// Pass to next layer of middleware
+	next();
+});
 
 const allRoutes = require('./routes');
 app.use('/api', allRoutes);
