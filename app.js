13be228d20dc38c794b7f74d7080000827b08960
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-10-18 12:46:02 +0200

    addedcors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv/config');
 require('./db');
 const express = require('express');
+var cors = require('cors');
+var app = express();
 
 const { isAuthenticated } = require('./middleware/jwt.middleware'); // <== IMPORT
 
@@ -10,12 +12,15 @@ require('./config')(app);
 // 👇 MIDDLEWARE MISSING
 const allRoutes = require('./routes');
 app.use('/api', allRoutes);
+app.use(cors());
 
 const authRouter = require('./routes/auth.routes');
 app.use('/api/auth', authRouter);
+app.use(cors());
 
 const phoneRoutes = require('./routes/phone.routes');
 app.use('/api', phoneRoutes);
+app.use(cors());
 
 // app.use((req, res, next) => {
 //     // If no routes match, send them the React HTML.
